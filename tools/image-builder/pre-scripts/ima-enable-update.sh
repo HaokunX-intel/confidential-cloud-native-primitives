94059828e38fd55c739d78954a326689865158c0
Xing, Haokun <haokun.xin@intel.com>
2023-12-07 20:14:02 +0800

    s2

--- tools/image-builder/pre-scripts/ima-enable-update.sh
+++ tools/image-builder/pre-scripts/ima-enable-update.sh
@@ -2,7 +2,7 @@
 
 GRUB_FILE=/etc/default/grub.d/50-cloudimg-settings.cfg
 GRUB_FILE_NEW=$GRUB_FILE".new"
-awk '{if($1 ~ /GRUB_CMDLINE_LINUX_DEFAULT/) sub("=\"","=\"ima_appraise=fix ima_policy=appraise_tcb "); print}' \
+awk '{if($1 ~ /GRUB_CMDLINE_LINUX_DEFAULT/) sub("=\"","=\"ima_appraise=fix "); print}' \
     $GRUB_FILE \
     > $GRUB_FILE_NEW
 mv $GRUB_FILE_NEW $GRUB_FILE
